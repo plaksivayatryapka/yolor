0b1a6791935e533d8de59b9d9da9072b8e004b35
plaksivaya_tryapka <boiler00@mail.ru>
2021-12-28 07:07:25 +0300

    minor

--- download_dataset.py
+++ download_dataset.py
@@ -4,5 +4,8 @@ import zipfile
 url = "https://morescience.app:443/charts/pylons.zip"
 urllib.request.urlretrieve(url, 'pylons.zip')
 
+url = "https://morescience.app:443/charts/yolor_p6.pt"
+urllib.request.urlretrieve(url, 'yolor_p6.pt')
+
 with zipfile.ZipFile('pylons.zip', 'r') as zip_ref:
     zip_ref.extractall('datasets')
