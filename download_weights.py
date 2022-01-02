c8358dec37032a06962f01e633b0faa08342df2f
plaksivaya_tryapka <boiler00@mail.ru>
2022-01-02 19:22:53 +0300

    added arguments

---
+++ download_weights.py
@@ -0,0 +1,15 @@
+import urllib.request
+import zipfile
+
+
+# print('downloading pylons dataset')
+# url = "https://morescience.app:443/charts/pylons.zip"
+# urllib.request.urlretrieve(url, 'pylons.zip')
+
+print('downloading weights')
+url = "https://morescience.app:443/charts/yolor_w6.pt"
+urllib.request.urlretrieve(url, 'yolor_w6.pt')
+
+# print('unpacking dataset')
+# with zipfile.ZipFile('pylons.zip', 'r') as zip_ref:
+#     zip_ref.extractall('datasets')
